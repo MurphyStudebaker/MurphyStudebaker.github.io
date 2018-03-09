7ddd94942a40969c4826727813c8d3ad3b07cb9a
MurphyStudebaker <murphy.stude@gmail.com>
2018-03-09 13:11:43 -0800

    Changed menu toggle to seperate functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 var hamburger = document.querySelector(".hamburger");
 var menu = document.querySelector(".menu");
 var content = document.querySelector(".content");
-var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 var active = true;
+var small = false;
 
 /* Run the setInit() function upon window load */
 if(window.addEventListener){
@@ -14,15 +14,12 @@ else{
 
 /* Width of viewport determines whether or not the menu begins as toggled*/
 function setInit() {
-  if (width <= 800) //portrait tablet or phone
+  setSize();
+  if (small) //portrait tablet or phone
   {
-    hamburger.classList.toggle("is-active"); //not active on load
-    menu.style.left = -330 + "px";
-    content.style.width = 100+"%";
-    content.style.left = 0;
+    hideMenu();
   } else {
-    content.style.left = 300+"px";
-    content.style.width = 75+'%';
+    showMenu();
   }
 }
 
@@ -30,14 +27,31 @@ function setInit() {
 hamburger.addEventListener("click", function() {
   hamburger.classList.toggle("is-active");
   if (active) {
-    active = false; //no longer active
-    menu.style.left = -330 + "px";
-    content.style.width = 100+"%";
-    content.style.left = 0;
+    hideMenu();
   } else {
-    active = true; //now active
-    menu.style.left = 0;
-    content.style.left = 300+"px";
-    content.style.width = 75+'%';
+    showMenu();
   }
 });
+
+function setSize() {
+  var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  if (width < 750) {
+    small = true;
+  } else {
+    small = false;
+  }
+}
+
+function showMenu() {
+  active = true; //now active
+  menu.style.left = 0;
+  content.style.left = 300+"px";
+  content.style.width = 75+'%';
+}
+
+function hideMenu() {
+  active = false; //no longer active
+  menu.style.left = -330 + "px";
+  content.style.width = 100+"%";
+  content.style.left = 0;
+}
