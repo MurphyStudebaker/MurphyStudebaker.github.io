a52d4a39a7c609b5022fb21d70e1ee63b2e807d8
MurphyStudebaker <murphy.stude@gmail.com>
2018-05-11 13:10:34 -0700

    Cleaned up design

--- scripts/main.js
+++ scripts/main.js
@@ -1,79 +1,26 @@
 var hamburger = document.querySelector(".hamburger");
 var nav = document.querySelector(".nav");
-var menu = document.querySelector(".menu");
-var content = document.querySelector(".content");
-var h2 = document.querySelector("h2");
-var active = true;
-var small = false;
+var active = false; //menu starts out hidden
 
-/* Run the setInit() function upon window load */
-if(window.addEventListener){
-    window.addEventListener('load',setInit,false); //W3C
-}
-else{
-    window.attachEvent('onload',setInit); //IE
-}
-
-/* Width of viewport determines whether or not the menu begins as toggled*/
-function setInit() {
-  setSize();
-  if (small) //portrait tablet or phone
-  {
-    hamburger.classList.toggle("is-active");
-    hideMenu();
-  } else {
-    showMenu();
-  }
-}
-
-/* When on mobile, hide menu when nav is clicked */
-nav.addEventListener("click", function() {
-  if (small) {
-    hamburger.classList.toggle("is-active");
-    hideMenu();
-  }
-});
 
 /* Controls the menu toggle */
 hamburger.addEventListener("click", function() {
   hamburger.classList.toggle("is-active");
   if (active) {
     hideMenu();
-    setSize(); //re-check size in case window is changed
   } else {
     showMenu();
   }
 });
 
-/* Checks browser width to adjust menu functionality */
-function setSize() {
-  var width = window.innerWidth;
-  //h2.innerHTML = width;
-  if (width < 985) {
-    small = true;
-  } else {
-    small = false;
-  }
-}
-
 /* Slides menu in from left */
 function showMenu() {
   active = true; //now active
-  menu.style.left = 0;
-  content.style.left = 300+"px";
-  content.style.width = 75+'%';
-  if (small) {
-    menu.style.width = 100 + "%";
-  }
+  nav.style.visibility = "visible";
 }
 
 /* Slides menu back off screen */
 function hideMenu() {
   active = false; //no longer active
-  menu.style.left = -330 + "px";
-  content.style.width = 100+"%";
-  content.style.left = 0;
-  if (small) {
-    menu.style.width = 250 + "px";
-  }
+  nav.style.visibility = "hidden";
 }
