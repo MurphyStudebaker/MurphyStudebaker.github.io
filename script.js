c3a2c5c5d5d61d8162a17d78d27fd437f02cd9ee
Murphy Studebaker <murphy.stude@gmail.com>
2024-10-08 22:53:17 -0700

    add links to portfolio

--- script.js
+++ script.js
@@ -31,33 +31,33 @@ const a4 = annotate(document.querySelector("#link-4"), {
 const ag = annotationGroup([a1, a2, a3, a4]);
 ag.show();
 
-var canvas = document.getElementById("canvas"),
-  context = canvas.getContext("2d"),
-  width = (canvas.width = window.innerWidth),
-  height = (canvas.height = window.innerHeightl);
+// var canvas = document.getElementById("canvas"),
+//   context = canvas.getContext("2d"),
+//   width = (canvas.width = window.innerWidth),
+//   height = (canvas.height = window.innerHeightl);
 
-var res = 10;
-var count = 1000;
+// var res = 10;
+// var count = 1000;
 
-for (var i = 0; i < count; ++i) {
-  var x = Math.random() * width,
-    y = Math.random() * height;
-  var value = getValue(x, y);
+// for (var i = 0; i < count; ++i) {
+//   var x = Math.random() * width,
+//     y = Math.random() * height;
+//   var value = getValue(x, y);
 
-  context.save();
-  context.translate(x, y);
-  render(value);
-  context.restore();
-}
+//   context.save();
+//   context.translate(x, y);
+//   render(value);
+//   context.restore();
+// }
 
-function getValue(x, y) {
-  return (x + y) * 0.01 * Math.PI * 2;
-}
+// function getValue(x, y) {
+//   return (x + y) * 0.01 * Math.PI * 2;
+// }
 
-function render(value) {
-  context.rotate(value);
-  context.beginPath();
-  context.moveTo(0, 0);
-  context.lineTo(res, 0);
-  context.stroke();
-}
+// function render(value) {
+//   context.rotate(value);
+//   context.beginPath();
+//   context.moveTo(0, 0);
+//   context.lineTo(res, 0);
+//   context.stroke();
+// }
